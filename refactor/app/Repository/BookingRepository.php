cfa3784a2214f25a006888240e9c0e388d6f14be
unknown <rendell.eyas@gmail.com>
2023-01-22 12:32:34 +0800

    added refactor update: long one line code, unutilized variable

--- refactor/app/Repository/BookingRepository.php
+++ refactor/app/Repository/BookingRepository.php
@@ -112,7 +112,7 @@ class BookingRepository extends BaseRepository
             $pagenum = "1";
         }
         $cuser = User::find($user_id);
-        $usertype = '';
+        $userType = '';
         $emergencyJobs = array();
         $noramlJobs = array();
         if ($cuser && $cuser->is('customer')) {
@@ -126,13 +126,12 @@ class BookingRepository extends BaseRepository
                 ->whereIn('status', ['completed', 'withdrawbefore24', 'withdrawafter24', 'timedout'])
                 ->orderBy('due', 'desc')
                 ->paginate(15);
-            $usertype = 'customer';
             return [
                 'emergencyJobs' => $emergencyJobs, 
                 'noramlJobs' => [], 
                 'jobs' => $jobs, 
                 'cuser' => $cuser, 
-                'usertype' => $usertype, 
+                'usertype' => 'customer', 
                 'numpages' => 0,
                 'pagenum' => 0
             ];
@@ -140,8 +139,6 @@ class BookingRepository extends BaseRepository
             $jobs_ids = Job::getTranslatorJobsHistoric($cuser->id, 'historic', $pagenum);
             $totaljobs = $jobs_ids->total();
             $numpages = ceil($totaljobs / 15);
-
-            $usertype = 'translator';
 
             $jobs = $jobs_ids;
             $noramlJobs = $jobs_ids;
@@ -151,7 +148,7 @@ class BookingRepository extends BaseRepository
                 'noramlJobs' => $noramlJobs, 
                 'jobs' => $jobs, 
                 'cuser' => $cuser, 
-                'usertype' => $usertype, 
+                'usertype' => 'translator', 
                 'numpages' => $numpages, 
                 'pagenum' => $pagenum
             ];
